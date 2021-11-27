e8def572086c38baf5416316ba50163fa9be3066
tanbirk <tanvir.kashyap879@gmail.com>
2021-11-27 13:21:32 +0530

    mid

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 //redux set up
 
@@ -11,7 +12,10 @@ import { store } from './store';
 export default function App() {
   return (
     <Provider store={store}> 
+    <SafeAreaProvider>
     <HomeScreen />
+    </SafeAreaProvider>
+    
     </Provider>
     
   );
